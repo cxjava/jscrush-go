221bec8a3aba8fbd31c560f18fe8c2c14e21cba4
Charles <cxjava@users.noreply.github.com>
2020-03-17 23:26:47 +0800

    first version

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/cxjava/jscrush-go
+
+go 1.14
+
+require (
+	github.com/dlclark/regexp2 v1.2.0 // indirect
+	github.com/dop251/goja v0.0.0-20200309191912-043cf4f34a48
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ gojav2/vm.go
@@ -0,0 +1,26 @@
+package gojav2
+
+import (
+	"fmt"
+	"github.com/dop251/goja"
+)
+
+var (
+	vm = goja.New()
+)
+
+func init() {
+	if _, err := vm.RunString(jscrush_src); err != nil {
+		fmt.Printf("jscrush_src init failed :%v \n", err)
+	}
+}
+
+func Jscrush(js string) string {
+	vm.Set("js_to_compress", js)
+	value, err := vm.RunString(`(function(js_to_compress) {return jscrush(js_to_compress) })(js_to_compress)`)
+	if err != nil {
+		fmt.Printf("Jscrush:%v\n", err)
+		return ""
+	}
+	return value.String()
+}
